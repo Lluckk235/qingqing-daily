c1fde34f6c045b8ec3c5dc7fb5a8ef1924d49180
Lluckk235 <1141506567@qq.com>
2026-08-02 12:09:35 +0800

    style: align challenge action buttons

--- js/modules/challenges.js
+++ js/modules/challenges.js
@@ -196,16 +196,18 @@ const Challenges = {
         <div class="challenge-module-header">
           <span class="challenge-module-title">${Dashboard.escapeHtml(item.text)}</span>
           <span class="challenge-module-count">${item.done}/${item.total}</span>
-          <span class="challenge-module-edit" data-edit="${item.id}" title="编辑">
-            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-            </svg>
-          </span>
-          <span class="challenge-module-del" data-del="${item.id}">
-            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>
-            </svg>
-          </span>
+          <div class="challenge-module-actions">
+            <span class="challenge-module-edit" data-edit="${item.id}" title="编辑">
+              <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+              </svg>
+            </span>
+            <span class="challenge-module-del" data-del="${item.id}">
+              <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/>
+              </svg>
+            </span>
+          </div>
         </div>
         <div class="challenge-dots">`;
 
